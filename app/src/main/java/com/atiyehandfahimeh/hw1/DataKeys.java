2b8729a940aa4b6376a82ff63dac7bb2934dade6
‫‪ce441-981-94105785 <fahimeh94tolouee@gmail.com>
2020-04-05 20:03:31 +0430

    Add dependacy

---
+++ app/src/main/java/com/atiyehandfahimeh/hw1/DataKeys.java
@@ -0,0 +1,7 @@
+package com.atiyehandfahimeh.hw1;
+
+public final class DataKeys {
+    private static final String FEATURES = "features";
+    private static final String PLACE_NAME = "place_name";
+    private static final String CENTER = "center";
+}
